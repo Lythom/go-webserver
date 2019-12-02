4aaa8b663455dc0cdf6489767b3695424f7e0b82
Lythom <sam_1er2002@yahoo.fr>
2019-12-02 15:48:59 +0100

    Initial commit

---
+++ server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
+)
+
+var key = securecookie.GenerateRandomKey(32)
+var store = sessions.NewCookieStore([]byte(key))
+
+func main() {
+
+	type User struct {
+		// Première lettre en majuscule obligatoire pour indiquer le caractère publie (exporté) du champ
+		Username string
+		Password string
+	}
+
+	http.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%f", rand.Float64())
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			var user User
+			dec := json.NewDecoder(r.Body)
+			// A supprimer. En cas de changement d'API (nouveaux champs) ça évitera de cracher le serveur
+			// dec.DisallowUnknownFields()
+			err := dec.Decode(&user)
+			if err != nil {
+				http.Error(w, "Bad Request", http.StatusBadRequest)
+				fmt.Fprintf(w, "%s", err.Error())
+			} else {
+				if user.Username == "theuser" && user.Password == "thepassword" {
+					session, _ := store.Get(r, "auth")
+					session.Values["authenticated"] = true
+					session.Save(r, w)
+					fmt.Fprintf(w, "OK")
+				} else if user.Username == "" || user.Password == "" {
+					http.Error(w, "Bad Request", http.StatusBadRequest)
+				} else {
+					http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				}
+
+			}
+		}
+		// Si pas implémenté, ne pas répondre. Supprimer ce code.
+		// else {
+		// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		// }
+	})
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "auth")
+		auth, ok := session.Values["authenticated"].(bool)
+		if !auth || !ok {
+			fmt.Fprintf(w, "Visiteur")
+		} else {
+			fmt.Fprintf(w, "Authentifié")
+		}
+	})
+
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "auth")
+		session.Values["authenticated"] = false
+		session.Save(r, w)
+		fmt.Fprintf(w, "Vous avez été deconnecté")
+	})
+
+	http.ListenAndServe(":1337", nil)
+}
